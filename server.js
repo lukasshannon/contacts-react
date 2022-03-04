e2d93e084eff910c96d2dcf2b8a451f849648f2a
Lukas Shannon <lukas.shannon@gmail.com>
2022-03-04 11:27:40 -0400

    connects MongoDB Atlas cloud database and defines server side routes

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+// Connect MongoDB Atlas Database
+connectDB();
+
+app.get('/', (req, res) => {
+  res.json({msg: 'API for contact-react accessed'})
+})
+
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
